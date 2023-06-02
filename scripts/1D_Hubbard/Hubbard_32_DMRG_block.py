6d25bc07af9561c014106f958a04b1942586d74a
Yannic Rath <yannic.rath@kcl.ac.uk>
2023-06-02 11:47:51 +0100

    Adjust bond dim schedule

--- scripts/1D_Hubbard/Hubbard_32_DMRG_block.py
+++ scripts/1D_Hubbard/Hubbard_32_DMRG_block.py
@@ -7,7 +7,7 @@ OBC = bool(int(sys.argv[2]))
 
 L = n_elec = 32
 
-driver = DMRGDriver(stack_mem=1 << 36, stack_mem_ratio=0.8, symm_type=SymmetryTypes.SU2)
+driver = DMRGDriver(stack_mem=1 << 40, stack_mem_ratio=0.8, symm_type=SymmetryTypes.SU2)
 driver.initialize_system(n_sites=L, n_elec=n_elec, spin=0)
 
 t = 1
@@ -32,11 +32,11 @@ b.add_term("((C+(C+D)0)1+D)0",
 mpo = driver.get_mpo(b.finalize(), iprint=2)
 
 ket = driver.get_random_mps(tag="GS", bond_dim=100, nroots=1)
-bond_dims = [100]*5 + [250] * 10 + [500] * 10 + [1000 + i * 100 for i in range(10)]
+bond_dims = [100]*5 + [250] * 10 + [500] * 10 + [1000 + i * 100 for i in range(16)]
 noises = [1e-4] * 5 + [1e-5] * 10 + [1e-6] * 10 + [0]
 
 
-en = driver.dmrg(mpo, ket, n_sweeps=1000, bond_dims=bond_dims, noises=noises, thrds=[1e-12]*40, iprint=1)
+en = driver.dmrg(mpo, ket, n_sweeps=1000, bond_dims=bond_dims, noises=noises, thrds=[1e-12]*46, iprint=1)
 
 if OBC:
     bc_string = "OBC"
