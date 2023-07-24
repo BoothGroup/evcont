91af5f6c86e45bd54aa9e20eebbd0eb3b5ee1871
Yannic Rath <yannic.rath@kcl.ac.uk>
2023-07-24 19:37:02 +0100

    Adjust training order

--- scripts/H_chain/H10_active_space_continuation.py
+++ scripts/H_chain/H10_active_space_continuation.py
@@ -77,7 +77,7 @@ MPI.COMM_WORLD.barrier()
 cascis = []
 
 overlap = one_rdm = two_rdm = None
-for trn_dist in [1.0, 3.6, 1.8, 1.2, 1.4, 1.6, 2.0, 2.4, 2.8, 3.2]:
+for trn_dist in [1.0, 3.6, 1.8, 2.6, 1.4, 3.2, 1.2, 1.6, 2.0, 2.8]:
     mol = get_mol(trn_dist)
     mf = scf.HF(mol)
     mf.kernel()
