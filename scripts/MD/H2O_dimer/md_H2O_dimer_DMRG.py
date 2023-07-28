a38ad8891c5d724713f348041cce72d34d74e6b2
Yannic Rath <yannic.rath@kcl.ac.uk>
2023-07-28 09:05:13 +0100

    Decrease tolerance threshold

--- scripts/MD/H2O_dimer/md_H2O_dimer_DMRG.py
+++ scripts/MD/H2O_dimer/md_H2O_dimer_DMRG.py
@@ -35,7 +35,7 @@ def dmrg_converge_fun(h1, h2, nelec, tag):
             np.copyto(h2[i, j, :, :], h2_slice)
 
     return converge_dmrg(
-        h1, h2, nelec, tag, tolerance=1.0e-4, mpi=MPI.COMM_WORLD.size > 1
+        h1, h2, nelec, tag, tolerance=1.0e-3, mpi=MPI.COMM_WORLD.size > 1
     )
 
 
