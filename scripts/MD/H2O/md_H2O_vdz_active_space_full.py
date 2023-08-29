7668bb96333ec57135a8c6deda639d3ed87168dc
Yannic Rath <yannic.rath@kcl.ac.uk>
2023-08-29 16:47:03 +0100

    Add double zeta H2O active space

---
+++ scripts/MD/H2O/md_H2O_vdz_active_space_full.py
@@ -0,0 +1,65 @@
+from pyscf import md, gto, scf, mcscf, lo, ao2mo, fci, lib, grad
+
+import numpy as np
+
+
+ncas = 12
+neleca = 6
+
+
+def get_mol(geometry):
+    mol = gto.Mole()
+
+    mol.build(
+        atom=[("H", geometry[0]), ("H", geometry[1]), ("O", geometry[2])],
+        basis="cc-pVDZ",
+        symmetry=False,
+        unit="Bohr",
+    )
+
+    return mol
+
+
+a_to_bohr = 1.8897259886
+
+stretch_factor = 1.2
+
+init_geometry = (
+    a_to_bohr
+    * stretch_factor
+    * np.array(
+        [
+            [0.0, 0.795, -0.454],
+            [0.0, -0.795, -0.454],
+            [0.0, 0.0, 0.113],
+        ]
+    )
+)
+
+
+mol = get_mol(init_geometry)
+
+
+init_mol = mol.copy()
+
+
+steps = 300
+dt = 5
+
+mf = scf.hf_symm.SymAdaptedRHF(init_mol.copy())
+scanner_fun = mcscf.CASCI(mf, ncas, neleca).nuc_grad_method().as_scanner()
+frames = []
+scanner_fun.mol = init_mol.copy()
+myintegrator = md.NVE(
+    scanner_fun,
+    steps=steps,
+    dt=dt,
+    incore_anyway=True,
+    frames=frames,
+    trajectory_output="active_space_trajectory_vtz.xyz",
+    energy_output="active_space_energy_vtz.xyz",
+)
+myintegrator.run()
+
+
+np.save("active_space_trajectory_vtz.npy", np.array([frame.coord for frame in frames]))
