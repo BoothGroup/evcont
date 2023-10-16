8ab4040bb91a20adf9b4b2bb278020faed401533
Kemal Atalar <k.atalar19@imperial.ac.uk>
2023-10-16 10:08:34 +0100

    Excited state gradients - initial implementation

--- evcont/ab_initio_gradients_loewdin.py
+++ evcont/ab_initio_gradients_loewdin.py
@@ -347,6 +347,11 @@ def get_energy_with_grad(mol, one_RDM, two_RDM, S, hermitian=True):
         grad_elec + grad.RHF(scf.RHF(mol)).grad_nuc(),
     )
 
+
+############################################################
+# NEW MULTISTATE - SEPERATED FROM REST FOR TESTING
+############################################################
+
 def get_multistate_energy_with_grad(mol, one_RDM, two_RDM, S, nroots=1, hermitian=True):
     """
     Calculates the potential energy and its gradient w.r.t. nuclear positions of a
@@ -374,19 +379,250 @@ def get_multistate_energy_with_grad(mol, one_RDM, two_RDM, S, nroots=1, hermitia
     h1 = np.linalg.multi_dot((ao_mo_trafo.T, scf.hf.get_hcore(mol), ao_mo_trafo))
     h2 = ao2mo.restore(1, ao2mo.kernel(mol, ao_mo_trafo), mol.nao)
 
-    #en, vec = approximate_ground_state(h1, h2, one_RDM, two_RDM, S, hermitian=hermitian)
     en, vec = approximate_multistate(h1, h2, one_RDM, two_RDM, S, nroots=nroots, hermitian=hermitian)
 
-    one_rdm_predicted = np.einsum("i,ijkl,j->kl", vec, one_RDM, vec, optimize="optimal")
-    two_rdm_predicted = np.einsum(
-        "i,ijklmn,j->klmn", vec, two_RDM, vec, optimize="optimal"
-    )
+    grad_elec_all = []
+    for i_state in range(nroots):
+        vec_i = vec[i_state,:]
 
-    grad_elec = get_grad_elec_OAO(
-        mol, one_rdm_predicted, two_rdm_predicted, ao_mo_trafo=ao_mo_trafo
-    )
+        one_rdm_predicted = np.einsum("i,ijkl,j->kl", vec_i, one_RDM, vec_i, optimize="optimal")
+        two_rdm_predicted = np.einsum(
+            "i,ijklmn,j->klmn", vec_i, two_RDM, vec_i, optimize="optimal"
+        )
+    
+        grad_elec = get_grad_elec_OAO(
+            mol, one_rdm_predicted, two_rdm_predicted, ao_mo_trafo=ao_mo_trafo
+        )
+        grad_elec_all.append(grad_elec)
+        
+    grad_elec_all = np.array(grad_elec_all)
 
     return (
         en.real + mol.energy_nuc(),
-        grad_elec + grad.RHF(scf.RHF(mol)).grad_nuc(),
+        grad_elec_all + grad.RHF(scf.RHF(mol)).grad_nuc(),
     )
+
+if __name__ == '__main__':
+    
+    # Some initial checks for the code
+    from pyscf import gto, fci
+    from evcont.FCI_EVCont import FCI_EVCont_obj
+    from evcont.ab_initio_eigenvector_continuation import approximate_multistate_OAO
+    from evcont.electron_integral_utils import get_basis, get_integrals
+    
+    from pyscf.mcscf import CASCI
+
+    nstate = 1 #1st excited state
+    nroots_evcont = 3
+    
+    natom = 10
+    
+
+    def get_mol(positions):
+        mol = gto.Mole()
+
+        mol.build(
+            atom=[("H", pos) for pos in positions],
+            basis="sto-6g",
+            symmetry=False,
+            unit="Bohr",
+        )
+
+        return mol
+    
+    equilibrium_dist = 1.78596
+
+    equilibrium_pos = np.array([(x * equilibrium_dist, 0.0, 0.0) for x in range(10)])
+
+    training_stretches = np.array([0.0, 0.5, -0.5, 1.0, -1.0])
+
+    trainig_dists = equilibrium_dist + training_stretches
+
+    #trainig_dists = [1.0, 1.8, 2.6]
+
+    continuation_object = FCI_EVCont_obj(nroots=nroots_evcont,
+                                         cibasis='canonical')
+    
+        
+    # Generate training data + prepare training models
+    for i, dist in enumerate(trainig_dists):
+        positions = [(x, 0.0, 0.0) for x in dist * np.arange(natom)]
+        mol = get_mol(positions)
+        continuation_object.append_to_rdms(mol)
+    
+    # Test the ground and excite state forces and gradients at training points
+    for i, dist in enumerate(trainig_dists):
+        positions = [(x, 0.0, 0.0) for x in dist * np.arange(natom)]
+        mol = get_mol(positions)
+        
+        # Predictions from mutistate continuation
+        en_continuation_ms, grad_continuation_ms = get_multistate_energy_with_grad(
+            mol,
+            continuation_object.one_rdm,
+            continuation_object.two_rdm,
+            continuation_object.overlap,
+            nroots=nstate+1
+        )
+
+        # Predictions from Hartree-Fock
+        hf_energy, hf_grad = mol.RHF().nuc_grad_method().as_scanner()(mol)
+
+        # Fci reference values
+        #en_exact, grad_exact = CASCI(mol.RHF(), natom, natom).nuc_grad_method().as_scanner()(mol)
+        
+        # Fci excited state reference values
+        mc = CASCI(mol.RHF(mol), natom, natom)
+        mc.fcisolver = fci.direct_spin0.FCI()
+        mc.fcisolver.nroots = nroots_evcont
+        ci_scan_exc = mc.nuc_grad_method().as_scanner(state=nstate)
+        ci_scan_0 = mc.nuc_grad_method().as_scanner(state=0)
+        
+        en_exc_exact, grad_exc_exact = ci_scan_exc(mol)
+        en_exact, grad_exact = ci_scan_0(mol)
+        
+        
+        # Checks
+        assert np.allclose(en_exact,en_continuation_ms[0])
+        assert np.allclose(grad_exact,grad_continuation_ms[0])
+
+        assert np.allclose(en_exc_exact,en_continuation_ms[nstate])        
+        assert np.allclose(grad_exc_exact,grad_continuation_ms[nstate])
+        
+    
+    '''
+    radius = 0.4
+    #radius_l = [0.1,0.4]
+    
+    n_data_points = 50
+    seed = 1
+    
+    norb = nelec = 10
+
+
+    
+    rng = np.random.default_rng(seed)
+
+
+
+    # Testing
+    en_exact_l = []; grad_exact_l = []
+    en_continuation_l = []; grad_continuation_l = []
+    
+    en_exc_exact_l = []; grad_exc_exact_l = []
+    en_continuation_ms_l = []; grad_continuation_ms_l = []
+    en_continuation_exc_l = []; grad_continuation_exc_l = []
+    
+    for i in range(n_data_points):
+        # Sample a new test geometry
+        displacement_theta = rng.random(size=(10)) * np.pi
+        displacement_phi = rng.random(size=(10)) * 2 * np.pi
+
+        sampled_displacement_x = (
+            radius * np.sin(displacement_theta) * np.cos(displacement_phi)
+        )
+        sampled_displacement_y = (
+            radius * np.sin(displacement_theta) * np.sin(displacement_phi)
+        )
+        sampled_displacement_z = radius * np.cos(displacement_theta)
+
+        sampled_displacement = np.stack(
+            (sampled_displacement_x, sampled_displacement_y, sampled_displacement_z),
+            axis=-1,
+        )
+
+        sampled_pos = equilibrium_pos + sampled_displacement
+        mol = get_mol(sampled_pos)
+
+        # Predictions from continuation
+        en_continuation, grad_continuation = get_energy_with_grad(
+            mol,
+            continuation_object.one_rdm,
+            continuation_object.two_rdm,
+            continuation_object.overlap,
+        )
+        
+        # Predictions from mutistate continuation
+        en_continuation_ms, grad_continuation_ms = get_multistate_energy_with_grad(
+            mol,
+            continuation_object.one_rdm,
+            continuation_object.two_rdm,
+            continuation_object.overlap,
+            nroots=nstate+1
+        )
+
+        # Predictions from Hartree-Fock
+        hf_energy, hf_grad = mol.RHF().nuc_grad_method().as_scanner()(mol)
+
+        # Fci reference values
+        en_exact, grad_exact = CASCI(mol.RHF(), 10, 10).nuc_grad_method().as_scanner()(mol)
+        
+        # Fci excited state reference values
+        mc = CASCI(mol.RHF(mol), 10, 10)
+        mc.fcisolver.nroots = nstate+2
+        ci_scan_exc = mc.nuc_grad_method().as_scanner(state=nstate)
+        #ci_scan_0 = mc.nuc_grad_method().as_scanner(state=0)
+        
+        en_exc_exact, grad_exc_exact = ci_scan_exc(mol)
+        #en_exact, grad_exact = ci_scan_0(mol)
+    
+        # Save
+        en_exact_l.append(en_exact)
+        grad_exact_l.append(grad_exact)
+        en_continuation_l.append(en_continuation)
+        grad_continuation_l.append(grad_continuation)
+        
+        en_continuation_ms_l.append(np.atleast_1d(en_continuation_ms)[0])
+        grad_continuation_ms_l.append(grad_continuation_ms[0,:,:])
+        
+        if nstate > 0:
+            en_continuation_exc_l.append(np.atleast_1d(en_continuation_ms)[nstate])
+            grad_continuation_exc_l.append(grad_continuation_ms[nstate,:,:])
+            en_exc_exact_l.append(en_exc_exact)
+            grad_exc_exact_l.append(grad_exc_exact)
+        
+    grad_continuation_l = np.array(grad_continuation_l)
+    grad_exact_l = np.array(grad_exact_l)
+    en_continuation_l = np.array(en_continuation_l)
+    en_exact_l = np.array(en_exact_l)
+    
+    grad_continuation_ms_l = np.array(grad_continuation_ms_l)
+    en_continuation_ms_l = np.array(en_continuation_ms_l)
+    grad_continuation_exc_l = np.array(grad_continuation_exc_l)
+    en_continuation_exc_l = np.array(en_continuation_exc_l)
+    
+    en_exc_exact_l = np.array(en_exc_exact_l)
+    grad_exc_exact_l = np.array(grad_exc_exact_l)
+
+    # 
+    assert np.allclose(grad_continuation_ms_l,grad_continuation_l)
+    
+    # Compute the differences
+    grad_diff = np.abs(grad_continuation_l - grad_exact_l)
+    en_diff = en_continuation_l - en_exact_l
+    en_diff_rel  = np.abs(en_diff/en_exact_l)
+    
+    grad_ms_diff = np.abs(grad_continuation_ms_l - grad_exact_l)
+    en_ms_diff = en_continuation_ms_l - en_exact_l
+    
+    grad_exc_diff = np.abs(grad_continuation_exc_l - grad_exc_exact_l)
+    en_exc_diff = en_continuation_exc_l - en_exc_exact_l
+    en_exc_diff_rel = np.abs(en_exc_diff/en_exc_exact_l)
+
+    
+    print(f'Force difference - ground state: {grad_diff.mean():.4e} ({grad_diff.min():.4e} - {grad_diff.max():.4e})')
+    print(f'Energy difference - ground state: {en_diff.mean():.4e} ({en_diff.min():.4e} - {en_diff.max():.4e})')
+    print(f'Energy difference - ground state: {en_diff_rel.mean():.4e} ({en_diff_rel.min():.4e} - {en_diff_rel.max():.4e})')
+    
+    print('-- Multistate')
+    print(f'Force difference - ground state: {grad_ms_diff.mean():.4e} ({grad_ms_diff.min():.4e} - {grad_ms_diff.max():.4e})')
+    print(f'Energy difference - ground state: {en_ms_diff.mean():.4e} ({en_ms_diff.min():.4e} - {en_ms_diff.max():.4e})')
+    
+    
+    print('-- Multistate - excited')
+    print(f'Force difference - excited state: {grad_exc_diff.mean():.4e} ({grad_exc_diff.min():.4e} - {grad_exc_diff.max():.4e})')
+    print(f'Energy difference - excited state: {en_exc_diff.mean():.4e} ({en_exc_diff.min():.4e} - {en_exc_diff.max():.4e})')
+    print(f'Relative energy difference - excited state: {en_exc_diff_rel.mean():.4e} ({en_exc_diff_rel.min():.4e} - {en_exc_diff_rel.max():.4e})')
+    
+    
+    '''
+    
