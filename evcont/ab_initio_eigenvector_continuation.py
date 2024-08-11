56d013c8623f891ac8b16699af75198fba91bf39
Yannic Rath <yannic.rath@npl.co.uk>
2024-08-11 20:24:21 +0100

    Allow for symmetry compressed two-RDMs in multistate prediction

--- evcont/ab_initio_eigenvector_continuation.py
+++ evcont/ab_initio_eigenvector_continuation.py
@@ -103,10 +103,44 @@ def approximate_multistate(h1, h2, one_RDM, two_RDM, S, nroots=1, hermitian=True
     Returns:
         Tuple[float, np.ndarray]: Energy approximation and ground state approximation.
     """
+
     # Calculate the Hamiltonian matrix
-    H = np.einsum("ijkl,kl->ij", one_RDM, h1, optimize="optimal") + 0.5 * np.einsum(
-        "ijklmn,klmn->ij", two_RDM, h2, optimize="optimal"
-    )
+
+    # Single electron part
+    H = np.tensordot(one_RDM, h1, axes=2)
+
+    # Two-electron contribution
+    if len(two_RDM.shape) == 6:
+        # No symmetries shape(two_RDM) = (a,b,i,j,k,l)
+        H += 0.5 * np.tensordot(two_RDM, h2, axes=4)
+
+    elif len(two_RDM.shape) == 5:
+        # Data symmetry only; shape(two_RDM) = (ab,i,j,k,l)
+        H_twobody = 0.5 * np.tensordot(two_RDM, h2, axes=4)
+        H[np.tril_indices(H.shape[0])] += H_twobody
+
+        if not hermitian:
+            H[np.triu_indices(H.shape[0])] = H.T.conj()[np.triu_indices(H.shape[0])]
+
+    elif len(two_RDM.shape) == 3:
+        # RDM electron exchange symmetry only; shape(two_RDM) = (a,b,ijkl)
+        h2_compressed = compress_electron_exchange_symmetry(h2, diag_multiplier=0.5)
+
+        H += two_RDM.dot(h2_compressed)
+
+    elif len(two_RDM.shape) == 2:
+        # RDM electron exchange symmetry + data symmetry; shape(two_RDM) = (ab,ijkl)
+
+        h2_compressed = compress_electron_exchange_symmetry(h2, diag_multiplier=0.5)
+
+        H_twobody = two_RDM.dot(h2_compressed)
+        H[np.tril_indices(H.shape[0])] += H_twobody
+
+        if not hermitian:
+            H[np.triu_indices(H.shape[0])] = H.T.conj()[np.triu_indices(H.shape[0])]
+
+    else:
+        assert False
 
     if hermitian is True:
         # Solve the generalized eigenvalue problem for Hermitian Hamiltonian
